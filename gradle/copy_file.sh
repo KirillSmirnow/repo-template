4fd873474b1795f811f8208fe0d15fbf470938f9
Kirill Smirnov <mr-skyu@yandex.ru>
2024-09-29 14:33:08 +0300

    Repo template generation

---
+++ gradle/copy_file.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+TEMPLATE_SUFFIX=".template"
+
+from="$1"
+to="$2"
+
+mkdir -p "$(dirname "$to")"
+
+if [[ "$from" = *"${TEMPLATE_SUFFIX}" ]]; then
+  to="$(dirname "$to")/$(basename "$to" "${TEMPLATE_SUFFIX}")"
+  eval "echo \"$(cat "$from")\"" >"$to"
+else
+  cp "$from" "$to"
+fi
--- gradle/generate.sh
+++ gradle/generate.sh
@@ -1,15 +1,34 @@
 #!/usr/bin/env bash
 
+get_latest_gradle_version() {
+  curl "https://services.gradle.org/distributions/" 2>/dev/null |
+    sed -nE 's/.*gradle-([0-9]+\.[0-9]+(\.[0-9]+)?)-bin.*/\1/p' |
+    head -n1
+}
+
+get_latest_lombok_version() {
+  curl "https://repo1.maven.org/maven2/io/freefair/gradle/lombok-plugin/maven-metadata.xml" 2>/dev/null |
+    sed -nE 's/.*<release>(.*)<\/release>.*/\1/p'
+}
+
 get_latest_spring_boot_version() {
   curl "https://repo1.maven.org/maven2/org/springframework/boot/spring-boot-parent/maven-metadata.xml" 2>/dev/null |
-    sed -nE 's/.*<latest>(.*)<\/latest>.*/\1/p'
+    sed -nE 's/.*<release>(.*)<\/release>.*/\1/p'
 }
 
 read -r -p "Project name: " project_name
-gradle_version="GV"
-lombok_version="LV"
-spring_boot_version=$(get_latest_spring_boot_version)
+gradle_version="$(get_latest_gradle_version)"
+lombok_version="$(get_latest_lombok_version)"
+spring_boot_version="$(get_latest_spring_boot_version)"
 
-source_path=$(dirname "$0")
-mkdir -p "${project_name}"
-cp -r "${source_path}/source/"* "${project_name}"
+export project_name
+export gradle_version
+export lombok_version
+export spring_boot_version
+
+source_path="$(dirname "$0")/source"
+target_path="${PWD}/${project_name}"
+(
+  cd "${source_path}" || exit
+  find . -type f -exec bash "../copy_file.sh" "{}" "${target_path}/{}" \;
+)
