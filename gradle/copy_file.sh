88c684d092989e3911b7cd65cc510545ca730861
Kirill Smirnov <mr-skyu@yandex.ru>
2024-09-29 16:41:54 +0300

    Fix templating: escape double quotes

--- gradle/copy_file.sh
+++ gradle/copy_file.sh
@@ -9,7 +9,7 @@ mkdir -p "$(dirname "$to")"
 
 if [[ "$from" = *"${TEMPLATE_SUFFIX}" ]]; then
   to="$(dirname "$to")/$(basename "$to" "${TEMPLATE_SUFFIX}")"
-  eval "echo \"$(cat "$from")\"" >"$to"
+  eval "echo \"$(cat "$from" | sed -r 's/"/\\"/g')\"" >"$to"
 else
   cp "$from" "$to"
 fi
